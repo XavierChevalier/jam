6a05733a139599346923801d258d0a1fc000fe6d
XavierChevalier <mcmodz@hotmail.fr>
2021-11-15 11:22:26 +0100

    feat: remove HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <img alt="Vue logo" src="./assets/images/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style lang="scss"></style>
