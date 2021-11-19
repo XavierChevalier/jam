0b862af96816631827d7369570daae8a8180d5f6
XavierChevalier <mcmodz@hotmail.fr>
2021-11-19 12:31:09 +0100

    test: fix snapshots tests from story

--- src/tests/tools/StorybookSnapshot.ts
+++ src/tests/tools/StorybookSnapshot.ts
@@ -1,24 +1,48 @@
 import { Story as StorybookStory, StoryContext } from '@storybook/vue3'
-import { ComponentOptionsWithObjectProps } from 'vue'
-import { mount } from '@vue/test-utils'
+import { ComponentOptionsWithObjectProps, ComponentPublicInstance } from 'vue'
+import { mount, VueWrapper } from '@vue/test-utils'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Story = StorybookStory<any>
 type StoryName = string
 type Stories = Record<StoryName, Story>
 
+const componentStoryWrapperId = 'COMPONENT_STORY_WRAPPER'
+
+/**
+ * When the component has multiple roots, the Test Utils view mount does not work due to these lines:
+ * https://github.com/vuejs/vue-test-utils-next/pull/25/files#diff-d9b07d77210db99e8eeee31f78da22c8176251496ef6a9497a726bddf8f017a1R27-R30
+ * Indeed, it tries to access the parent of the element to render it.
+ * However, there is no parent in the case of a simple story.
+ * To fix it, we wrap the template.
+ */
+function wrapTemplate(
+  component: ComponentOptionsWithObjectProps
+): ComponentOptionsWithObjectProps {
+  return {
+    ...component,
+    template: `<div id="${componentStoryWrapperId}">${component.template}</div>`,
+  }
+}
+
 function createComponentFromStory(story: Story) {
-  return story(
+  const component = story(
     story.args,
     {} as StoryContext
   ) as ComponentOptionsWithObjectProps
+
+  return wrapTemplate(component)
+}
+
+function getUnwrappedElement(wrapper: VueWrapper<ComponentPublicInstance>) {
+  return wrapper.find(`#${componentStoryWrapperId} > *`).element
 }
 
 function generateStorybookSnapshotTest(storyName: StoryName, story: Story) {
   it(`should render the "${storyName}" component correctly`, () => {
     const wrapper = mount(createComponentFromStory(story))
 
-    expect(wrapper.element).toMatchSnapshot()
+    expect(getUnwrappedElement(wrapper)).toMatchSnapshot()
   })
 }
 
