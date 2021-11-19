200e83893905395462ba5048479567f18f11cf84
XavierChevalier <mcmodz@hotmail.fr>
2021-11-19 16:02:07 +0100

    build: ignore .js any import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import * as alias from './build/aliases'
 
 // https://vitejs.dev/config/
