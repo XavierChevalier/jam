b1ee0bc598c35aad6e3f9da2210df432cd8eefc7
XavierChevalier <mcmodz@hotmail.fr>
2021-11-26 18:50:37 +0100

    feat(storybook): add all viewports and add true white background color

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,25 @@
-const tailwindcss = require('../tailwind.config')
-require('./../src/assets/styles/index.scss')
+// noinspection NpmUsedModulesInstalled
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import tailwindcss from '../tailwind.config'
+import './../src/assets/styles/index.scss'
 
 export const parameters = {
-  actions: {argTypesRegex: '^on[A-Z].*'},
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
   backgrounds: {
     default: 'light',
     values: [
-      {name: 'light', value: tailwindcss.theme.colors.white.dark},
-      {name: 'dark', value: tailwindcss.theme.colors.black.light},
+      { name: 'true-white', value: '#FFF' },
+      { name: 'light', value: tailwindcss.theme.colors.white.dark },
+      { name: 'dark', value: tailwindcss.theme.colors.black.light },
     ],
   },
 }
