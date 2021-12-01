d94ac5be4ba88a398287085cd4de36ba1c56bac3
XavierChevalier <mcmodz@hotmail.fr>
2021-12-01 11:24:49 +0100

    test(jest): fail only on invalid prop type check Vue warning

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -1,13 +1,15 @@
-function isVueWarn(message: string) {
-  return message.startsWith('[Vue warn]')
+function isVueWarnForFailedPropTypeCheck(message: string) {
+  return message.startsWith(
+    '[Vue warn]: Invalid prop: type check failed for prop'
+  )
 }
 
-const failOnVueWarn = (message: string) => {
-  if (isVueWarn(message)) throw new Error(message)
+const failOnVueWarn = (message: string, args: unknown[]) => {
+  if (isVueWarnForFailedPropTypeCheck(message)) throw new Error(args.join(''))
 }
 
 const warn = console.warn
 console.warn = (...args) => {
   warn.apply(console, args) // keep default behaviour
-  failOnVueWarn(args[0])
+  failOnVueWarn(args[0], args)
 }
